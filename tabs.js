c84b6f93035234afcdfce6ab4e2967728032fb8d
Erdem Elvan <erdemelvan35@gmail.com>
2022-06-13 02:37:03 +0300

    keyboard navigation for the tabs

--- tabs.js
+++ tabs.js
@@ -1,14 +1,33 @@
 const tabList = document.querySelector('[role="tablist"]');
 const tabs = tabList.querySelectorAll('[role="tab"]');
 
-tabList.addEventListener('keydown', (e) => {
-    console.log(e.keyCode);
-    const keydownLeft = 37;
-    const keydownRight = 39;
+tabList.addEventListener("keydown", changeTabFocus);
 
-    // change the tabindex of the current tab to -1
-    
-    // if the right key is pushed, move to the next tab on the right
-    
-    // if the left key is pushed, move to the next tab on the left
-})
+let tabFocus = 0;
+function changeTabFocus(e) {
+  /* console.log(e.keyCode); */
+  const keydownLeft = 37;
+  const keydownRight = 39;
+
+  // change the tabindex of the current tab to -1
+  if (e.keyCode === keydownLeft || e.keyCode === keydownRight) {
+    tabs[tabFocus].setAttribute("tabindex", -1);
+  }
+  // if the right key is pushed, move to the next tab on the right
+  if (e.keyCode === keydownRight) {
+    tabFocus++;
+    if (tabFocus >= tabs.length) {
+      tabFocus = 0;
+    }
+  }
+  // if the left key is pushed, move to the next tab on the left
+  if (e.keyCode === keydownLeft) {
+    tabFocus--;
+    if (tabFocus < 0) {
+      tabFocus = tabs.length - 1;
+    }
+  }
+
+  tabs[tabFocus].setAttribute("tabindex", 0);
+  tabs[tabFocus].focus();
+}
